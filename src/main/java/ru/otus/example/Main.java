1a8ae1c8c206b1a5ecd4e6c112262fcd941fd667
Vladimir Andreev <vladimirandreev1987@bk.ru>
2023-04-23 00:02:31 +0200

    Changed. Basic example.

--- src/main/java/ru/otus/example/Main.java
+++ src/main/java/ru/otus/example/Main.java
@@ -1,9 +1,23 @@
 package ru.otus.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Integer> example = new ArrayList<>();
+
+        int min = 0;
+        int max = 100;
+
+        for (int i = min; i < max; i++) {
+            example.add(i);
+        }
+
+        System.out.println(example);
+
     }
 
 }
